51379ddbb19eb46a1c994b797a3928e395cf3c54
hao <hao@sporule.com>
2020-10-07 18:40:18 +0100

    added loffer template as default

---
+++ template/customPages/index.js
@@ -0,0 +1,15 @@
+
+import Posts from "./posts";
+
+const CustomPages = {
+    "home":{
+        "title":"Home",
+        "component": Posts
+    },
+    "posts": {
+        "title": "Posts",
+        "component": Posts
+    }
+}
+
+export default CustomPages;
